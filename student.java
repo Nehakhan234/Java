05563722870dcb5c61c6f1cf993458eea9ba23ac
Neha khan <neha.zohra123@gmail.com>
2017-04-26 00:18:20 +0530

    Inheritance

--- student.java
+++ student.java
@@ -1,67 +1,79 @@
-import java.io.*;
-import java.lang.*;
-import java.util.*;
+package inheritance;
 
+import java.util.Scanner;
+class student {
 
-interface person
- {
-    void set_data(String name,int age);
- }
+	
+	Scanner in =new Scanner(System.in);
+	//	in = new Scanner(System.in);
+	String name,no;
+	int m1,m2,m3,m4;
+	void get_data()
+	{
+		
+		
+			System.out.println("Enter the name");
+			name = in.next();
+			System.out.println("Enter the reg no");
+			no = in.next();
+			System.out.println("Enter marks 1");
+			m1 = in.nextInt();
+			System.out.println("Enter marks 2");
+			m2 = in.nextInt();
+			System.out.println("Enter marks 3");
+			m3 = in.nextInt();
+			System.out.println("Enter marks 4");
+			m4 = in.nextInt();
+		
+		//catch(Exception e)
+		{
+			//System.out.println("Error");
+		}
+	}
+	void put_data()
+	{
+		 System.out.println("Student details are: ");
+		 System.out.println("Name = "+name);
+		 System.out.println("Reg no = "+no);
+		 System.out.println("Marks 1 = "+m1);
+		 System.out.println("Marks 2 = "+m2);
+		 System.out.println("Marks 3 = "+m3);
+		 System.out.println("Marks 4 = "+m4);
+	}
 
+}
 
- class student implements person
-   {
-       private String name;
-       private int age;
-       public void set_data(String name,int age)
-       {
-    	   this .name=name;
-    	   this .age=age;
-    	   System.out.println("Students Details:\nStudent Name="+name+"\nAge="+age);
+class inherit extends student
+{
+	float total;
+	void tot_marks()
+	{
+		
+		total=m1+m2+m3+m4;
+		System.out.println("Total marks = "+total);
+	}
+}
+class multi extends inherit
+{
+	
+	float avg;
+	void average()
+	{
+		System.out.println("Grade = "+total/10.0);
+		avg = total/4;
+       System.out.println("Average is "+avg);
+    }
+}
+class stud
+{
+	public static void main(String args[])
+	{
+		//inherit obj = new inherit();
+		multi a = new multi();
+		a.get_data();
+		a.put_data();
+		a.tot_marks();
+		a.average();
+	}
+}
 
-       }
-  }
- 
- 
- class teacher implements person
- {
-       private String name;
-       private int age;
-       public void set_data(String name,int age)
-       {
-    	   this .name=name;
-    	   this .age=age;
-    	   System.out.println("Teacher Details:\nTeacher Name="+name+"\nAge="+age);
-       }
- }
- 
- 
-  class interface_demo
-  {
-	  public static void main(String args[]) throws Exception
-  {
-      String name;
-      int age;
-      person p;
-      
-      teacher t=new teacher();
-      student s=new student();
-      
-      BufferedReader obj=new BufferedReader(new InputStreamReader(System.in));
-      p=t;
-      System.out.println("Enter the name of the Teacher:");
-      name=obj.readLine();
-      
-      System.out.println("Enter the Age of the Teacher:");
-      age=Integer.parseInt(obj.readLine());
-      p.set_data(name,age);
-      p=s;
-      
-      System.out.println("Enter the name of the Student:");
-      name=obj.readLine();
-      
-      System.out.println("Enter the Age of the Student:");
-      age=Integer.parseInt(obj.readLine());
-      p.set_data(name,age);
-  	}
-  }
